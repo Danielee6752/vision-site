e75ef93b611416b21355597f6934cd40146fe549
Danielee6752 <160693934+Danielee6752@users.noreply.github.com>
2026-04-29 17:07:16 +0900

    Add files via upload

---
+++ flarelane-worker.js
@@ -0,0 +1,4 @@
+// FlareLane Web SDK Service Worker
+// 이 파일은 반드시 사이트 루트(/flarelane-worker.js)에 위치해야 합니다.
+// (예: https://your-domain.com/flarelane-worker.js 로 접근 가능해야 함)
+importScripts('https://cdn.flarelane.com/WebSDKWorker.js');
